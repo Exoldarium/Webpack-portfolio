111a37a4628a5706461c553ef7ddfd0d1eeb9240
Dusan <shandoo91@gmail.com>
2022-12-23 17:45:30 +0100

    image path

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     maxAssetSize: 512000
   },
   output: {
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, './build'),
     filename: 'bundle.js',
   },
   target: 'web',
