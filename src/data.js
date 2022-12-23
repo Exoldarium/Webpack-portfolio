46b950782791ce9cf05d5716d5a0837f9020ee58
Dusan <shandoo91@gmail.com>
2022-12-23 15:47:46 +0100

    fixed image patsh, added dotenv

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ export const projects = [
     title: "Game Deals Searching App",
     description:
       "Game deals searching app using CheapShark API, made completely from scratch without using any frameworks, libraries or compilers. Just pure vanilla JavaScript, CSS and HTML",
-    image: process.env.PUBLIC_URL + "images/animation.gif",
+    image: "./images/animation.gif",
     link: "https://exoldarium.github.io/Game-Deals-Page/",
   },
 
@@ -11,7 +11,7 @@ export const projects = [
     title: "Super Cool Shoes",
     description: 
       "A product page built using React. The user can add and delete product from the cart. The cart data persists on page reload. User can also browse through pictures or enlarge the selected picture from the gallery. It's built using responsive design and accesibility in mind.",
-    image: process.env.PUBLIC_URL + "images/sneakersAnimation.gif",
+    image: "./images/sneakersAnimation.gif",
     link: "https://exoldarium.github.io/Super-Cool-Sneakers/"
   }
 ];
@@ -20,10 +20,10 @@ export const skills = ['JavaScript' , "HTML", "CSS", "React", "Git"];
 
 export const socialImages = [
 {
-  linkedinImage: process.env.PUBLIC_URL + "images/linkedin.svg",
-  githubImage: process.env.PUBLIC_URL + "images/github.svg",
+  linkedinImage: "./images/linkedin.svg",
+  githubImage: "./images/github.svg",
   linkedinLink: 'https://www.linkedin.com/in/dusan-milosavljevic-675ab5230/',
   githubLink: 'https://github.com/Exoldarium',
-  mailImage: process.env.PUBLIC_URL + "images/mail.svg"
+  mailImage: "./images/mail.svg"
 }
 ];
